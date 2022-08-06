e8268433689c11d82f256a55ea07c781e3fdd6ed
oshah97 <oshah97@outlook.com>
2022-08-06 16:44:43 -0500

    subnet

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
+# VPC
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "main"
+  }
+}
+
+# Subnet
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "eu-west-1"
+
+  tags = {
+    Name = "Default subnet for eu-west-1"
   }
 }
